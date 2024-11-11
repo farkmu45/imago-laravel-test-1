5693daf634b6ab2f64cbbb889cc0468ebcaf5da0
farkmu45 <farukmicros@outlook.com>
2024-11-11 09:39:20 +0700

    Change welcome page text into more readable format

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,2 +1,11 @@
 <x-app-layout>
+    @guest
+    <h1 class="text-center mt-32 text-3xl font-bold text-gray-800">
+        Hello, guest, you're not authenticated.
+      </h1>
+    @else
+      <h1 class="text-center mt-32 text-3xl font-bold text-gray-800">
+        Hello, {{ Auth::user()->username }}, you're authenticated.
+      </h1>
+    @endguest
 </x-app-layout>
